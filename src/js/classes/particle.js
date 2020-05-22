e2a704264b456062482c9e9872550d0a91c6330e
Jesse Dijkman <jesse.dijkman@hva.nl>
2020-05-22 16:14:49 +0200

    Added particle class

--- src/js/classes/particle.js
+++ src/js/classes/particle.js
@@ -1,11 +1,43 @@
-import { ctx } from "../context.js"
+import { ctx, canvas } from "../context.js"
+
+// Temporaray constants
+const GRAVITY = 0
+const FRICTION = 0.9
 
 export default class Particle {
-  constructor({ x, y, radius, color }) {
+  constructor({ x, y, vx, vy, radius, color }) {
     this.x = x
     this.y = y
+    this.vx = vx
+    this.vy = vy
     this.radius = radius
     this.color = color
+    this.deprecation = 0.025
+  }
+
+  update() {
+    if (
+      this.y - this.radius + this.vy <= 0 ||
+      this.y + this.radius + this.vy >= canvas.height
+    ) {
+      this.vy = -this.vy * FRICTION
+      this.vx = this.vx * FRICTION
+    } else {
+      this.vy += GRAVITY
+    }
+
+    if (
+      this.x - this.radius + this.vx <= 0 ||
+      this.x + this.radius + this.vx >= canvas.width
+    ) {
+      this.vx = -this.vx * FRICTION
+      this.vy = this.vy * FRICTION
+    }
+
+    this.y += this.vy
+    this.x += this.vx
+
+    this.draw()
   }
 
   draw() {
