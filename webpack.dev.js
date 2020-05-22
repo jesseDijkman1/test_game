30de2b027f6206f99b5132346ba8fb4a28a900af
Jesse Dijkman <jesse.dijkman@hva.nl>
2020-05-22 16:13:35 +0200

    Extra config webpack dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -21,4 +21,22 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      modules: false,
+      reasons: false,
+      children: false,
+      source: false,
+      errors: true,
+      errorDetails: true,
+      warnings: true,
+      publicPath: false,
+    },
+  },
 }
